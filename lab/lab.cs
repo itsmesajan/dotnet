eb7d42530a75d7bf82c3d3f8e98d9f01459774aa
Sajan Maharjan <itsmesajanmaharjan@gmail.com>
2024-11-15 19:35:39 +0545

    method overrride

---
+++ lab/lab.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Text;
+namespace Text
+{
+  class Animal
+  {
+    public virtual void Eat()
+    {
+      Console.WriteLine("Animal is eat grass and flesh");
+    }
+  }
+}
+
+class Cow : Animal
+{
+  public override void Eat()
+  {
+    Console.WriteLine("Cow is eat grass only.");
+  }
+
+  static void Main(string[] args)
+  {
+    Cow obj = new Cow();
+    obj.Eat();
+
+    Animal obj1 = new Cow();
+    obj1.Eat();
+  }
+}
