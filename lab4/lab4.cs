71724826ef53caab9ecc6a5a1ca21fe0beb61ccc
Sajan Maharjan <itsmesajanmaharjan@gmail.com>
2024-11-16 21:53:05 +0545

    constructor

---
+++ lab4/lab4.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConstructorDemo
+{
+  class MyClass
+  {
+    private int number;
+    public MyClass()
+    {
+      Console.WriteLine("Default constructor is called.");
+    }
+    public MyClass(int number)
+    {
+      this.number = number;
+      Console.WriteLine("Parameterized constructor is called Number = " + number);
+    }
+
+    private MyClass(string message)
+    {
+      Console.WriteLine("Private constructor is called. Message = " + message);
+    }
+
+    //Static method to exit the private constructor
+
+    public static MyClass CreateInstance(string message)
+    {
+      return new MyClass(message);
+    }
+
+    class Program
+    {
+      static void Main(string[] args)
+      {
+        MyClass obj1 = new MyClass();
+        MyClass obj2 = new MyClass(10);
+        MyClass obj3 = MyClass.CreateInstance("Hello world!");
+        Console.ReadLine();
+      }
+    }
+  }
+}
