1600342b224f1ed7bb4d41342fe392017cdc5c05
Sajan Maharjan <itsmesajanmaharjan@gmail.com>
2024-11-17 21:02:13 +0545

    Multilevel Inheritance

---
+++ lab5/lab5-1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace InheritanceDemo
+{
+  // Base class
+  class GrandParent
+  {
+    public void DisplayGrandParent()
+    {
+      Console.WriteLine("This is the GrandParent class.");
+    }
+  }
+
+  // Derived class
+  class Parent : GrandParent
+  {
+    public void DisplayParent()
+    {
+      Console.WriteLine("This is the Parent class.");
+    }
+  }
+
+  // Another derived class (from Parent)
+  class Child : Parent
+  {
+    public void DisplayChild()
+    {
+      Console.WriteLine("This is the Child class.");
+    }
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Child childObj = new Child();
+
+      // Access methods from all levels of the inheritance hierarchy
+      childObj.DisplayGrandParent();
+      childObj.DisplayParent();
+      childObj.DisplayChild();
+
+      Console.ReadLine();
+    }
+  }
+}
