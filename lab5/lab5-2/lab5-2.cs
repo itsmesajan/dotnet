7887423a07f0788a3980167d1546ca688b6fa335
Sajan Maharjan <itsmesajanmaharjan@gmail.com>
2024-11-17 21:18:53 +0545

    Multiple Inheritance Using Interfaces update

--- lab5/lab5-2/lab5-2.cs
+++ lab5/lab5-2/lab5-2.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace InheritanceDemo
+{
+    // Interface 1
+    interface IAnimal
+    {
+        void Eat();
+    }
+
+    // Interface 2
+    interface IBird
+    {
+        void Fly();
+    }
+
+    // Class implementing both interfaces
+    class Bat : IAnimal, IBird
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Bat is eating.");
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine("Bat is flying.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bat bat = new Bat();
+
+            // Access methods from both interfaces
+            bat.Eat();
+            bat.Fly();
+
+            Console.ReadLine();
+        }
+    }
+}
