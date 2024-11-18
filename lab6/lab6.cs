d80ca22b23939269f6f8e8e39db3097495aca4c0
Sajan Maharjan <itsmesajanmaharjan@gmail.com>
2024-11-18 22:07:14 +0545

    abstract class

--- lab6/lab6.cs
+++ lab6/lab6.cs
@@ -1,1 +1,39 @@
-﻿
+﻿using System;
+public abstract class Animal
+{
+  public abstract void MakeSound();
+  public void Sleep()
+  {
+    Console.WriteLine("Sleeping Zzz...");
+  }
+}
+
+public class Dog : Animal
+{
+  public override void MakeSound()
+  {
+    Console.WriteLine("Woof!");
+  }
+}
+
+public class Cat : Animal
+{
+  public override void MakeSound()
+  {
+    Console.WriteLine("Meow!");
+  }
+}
+
+class program
+{
+  static void Main(string[] args)
+  {
+    Animal dog = new Dog();
+    dog.MakeSound();
+    dog.Sleep();
+
+    Animal cat = new Cat();
+    cat.MakeSound();
+    cat.Sleep();
+  }
+}
